711510e0e0a1f6f642687542be2a9ec6e1f7cfa5
Ritisha Sharma <ritishas2000@gmail.com>
2023-01-13 18:28:52 -0800

    Function to add team members

---
+++ src/pokehelpyer/add_team_members.py
@@ -0,0 +1,15 @@
+"""Add pokemon to team members.
+    
+    Parameters
+    ----------
+    pokemon_names : list
+        List of names given by users of the 5 team members.
+    
+    Returns
+    -------
+    pokemon_types
+        List of types of the given pokemon names.
+    """
+def add_team_members(pokemon_names):
+   
+ ...
