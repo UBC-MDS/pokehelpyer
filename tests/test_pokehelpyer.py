60680e4979a8998348003b6579a597742fc343d6
Ritisha Sharma <ritishas2000@gmail.com>
2023-01-17 20:56:32 -0800

    Add tests for get_types function

--- tests/test_pokehelpyer.py
+++ tests/test_pokehelpyer.py
@@ -1,1 +1,7 @@
-from pokehelpyer import pokehelpyer
+from pokehelpyer.pokehelpyer import get_types
+
+def test_get_types():
+    """Test get_types function returns correct result for different inputs."""
+    assert get_types(["RaICHu", "zubAt"]) == [["Electric"], ["Poison", "Flying"]], "Not able to handle improper capitalization"
+    assert get_types(["Snorlax!!", "...Chingling"]) == [["Normal"], ["Psychic"]], "Not able to handle improper punctuation"
+    assert get_types(["Mime Jr.", "Porygon-Z", "Nidoran♀"]) == [["Psychic", "Fairy"], ["Normal"], ["Poison"]], "Not able to handle Pokemon with symbol in the name"
