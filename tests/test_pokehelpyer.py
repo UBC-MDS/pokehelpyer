6dc62f5a5e674a1a05805671bf26ab3ce0ba6386
Sneha Sunil <snehasunilgm@gmail.com>
2023-01-19 16:35:52 -0800

    tests added

--- tests/test_pokehelpyer.py
+++ tests/test_pokehelpyer.py
@@ -15,3 +15,8 @@ def test_calc_weaknesses_invalid_result():
     actual_dict = calc_weaknesses([['Steel', 'Flying'], ['Grass', 'Ice']])
     expected_dict = {'Normal': 0, 'Fire': 0, 'Water': 1, 'Electric': 1, 'Grass': 0, 'Ice': 0, 'Fighting': 1, 'Poison': 1, 'Ground': 1, 'Flying': 1, 'Psychic': 0, 'Bug': 1, 'Rock': 2, 'Ghost': 0, 'Dragon': 0, 'Dark': 0, 'Steel': 1, 'Fairy': 0}
     assert actual_dict != expected_dict
+
+def test_calc_weaknesses_invalid_input_list():
+    """Tests the calculate weaknesses function for empty input list"""
+    actual_val = calc_weaknesses([])
+    assert actual_val == None
