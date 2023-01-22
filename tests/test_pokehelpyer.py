59ad4f45d39f2c44c46e0bde21c287cdd3631692
Jakob Thoms <j99thoms@gmail.com>
2023-01-21 19:50:23 -0800

    tests: add tests for calc_balance

--- tests/test_pokehelpyer.py
+++ tests/test_pokehelpyer.py
@@ -76,3 +76,32 @@ def test_recommend():
     assert isinstance(actual, str), "recommend should handle user input error for teams with only one pokemon."
     expected = recommend(['Pikachu'])
     assert actual == expected, "recommend should handle user input error for teams with only one pokemon."
+
+
+# calc_balance:
+
+def test_calc_balance():
+    zero_resistances = {'Normal': 0, 'Fire': 0, 'Water': 0, 'Electric': 0, 'Grass': 0, 'Ice': 0, 'Fighting': 0,
+     'Poison': 0, 'Ground': 0, 'Flying': 0, 'Psychic': 0, 'Bug': 0, 'Rock': 0, 'Ghost': 0, 'Dragon': 0,
+      'Dark': 0, 'Steel': 0, 'Fairy': 0}
+    zero_weaknesses = zero_resistances
+    fire_water_resistances = {'Normal': 0, 'Fire': 1, 'Water': 1, 'Electric': 0, 'Grass': 0, 'Ice': 0, 'Fighting': 0,
+     'Poison': 0, 'Ground': 0, 'Flying': 0, 'Psychic': 0, 'Bug': 0, 'Rock': 0, 'Ghost': 0, 'Dragon': 0,
+      'Dark': 0, 'Steel': 0, 'Fairy': 0}
+    fire_water_weaknesses = fire_water_resistances
+    all_resistances = {'Normal': 1, 'Fire': 1, 'Water': 1, 'Electric': 1, 'Grass': 1, 'Ice': 1, 'Fighting': 1,
+     'Poison': 1, 'Ground': 1, 'Flying': 1, 'Psychic': 1, 'Bug': 1, 'Rock': 1, 'Ghost': 1, 'Dragon': 1,
+      'Dark': 1, 'Steel': 1, 'Fairy': 1}
+    all_weaknesses = all_resistances
+
+    """Test `calc_balance` returns a float."""
+    assert isinstance(calc_balance(zero_resistances, zero_weaknesses,), float), "calc_balance should return a float."
+    assert isinstance(calc_balance(fire_water_resistances, fire_water_weaknesses), float), "calc_balance should return a float."
+
+    """Test `calc_balance` returns reasonable values."""
+    assert calc_balance(zero_resistances, zero_weaknesses) == 0, "calc_balance should return zero for a team with no resistances nor weaknesses."
+    assert calc_balance(fire_water_resistances, fire_water_weaknesses) == 0, "calc_balance should return zero for a team with equal and opposite resistances and weaknesses."
+    assert calc_balance(fire_water_resistances, zero_weaknesses) > 0, "calc_balance should return a positive number for a team with resistances and no weaknesses."
+    assert calc_balance(all_resistances, zero_weaknesses) > calc_balance(fire_water_resistances, zero_weaknesses), "calc_balance should return higher numbers for teams with more resistances."
+    assert calc_balance(zero_resistances, fire_water_weaknesses) < 0, "calc_balance should return a negative number for a team with weaknesses and no resistances."
+    assert calc_balance(zero_resistances, all_weaknesses) < calc_balance(zero_resistances, fire_water_weaknesses), "calc_balance should return lower numbers for teams with more weaknesses."
